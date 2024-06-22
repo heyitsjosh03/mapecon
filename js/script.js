c5c3e248c9a16e04d008bb192bbd6d28153bc248
Joshua <panambitanjoshuam@gmail.com>
2024-06-22 10:59:09 +0800

    Revert "push"
    This reverts commit bd3c487fcd3380b1861f1dbce3f0148d1d7a6150.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+if (localStorage.getItem('isLoggedInApprover') === 'true') {
+    // Redirect the user to the Approver interface
+    window.location.href = '../Approver Interface/Approver home.php';
+} else {
+    // If not logged in, proceed with regular login process
+    // Example login code here...
+
+    // Assuming login is successful and the user is an Approver
+    if (userStatus === 'Approver') {
+        // Set a flag indicating the user is logged in as an Approver
+        localStorage.setItem('isLoggedInApprover', 'true');
+
+        // Redirect the user to the Approver interface
+        window.location.href = '../Approver Interface/Approver home.php';
+    } else {
+        // Handle other login scenarios if needed
+    }
+}
+
