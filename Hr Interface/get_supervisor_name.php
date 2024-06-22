80bf122435d88865893b5caee5b5f60d8e6de255
Joshua <panambitanjoshuam@gmail.com>
2024-06-22 10:30:26 +0800

    Fixed the error message in Supervisor in Edit User

--- Hr Interface/get_supervisor_name.php
+++ Hr Interface/get_supervisor_name.php
@@ -5,13 +5,13 @@ if (isset($_POST['approver_id'])) {
     $approver_id = $_POST['approver_id'];
     $query = "SELECT firstname, lastname FROM users WHERE user_id = $approver_id";
     $result = mysqli_query($connection, $query);
-    if ($result) {
+    if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
         echo htmlspecialchars($row['firstname'] . ' ' . $row['lastname']);
     } else {
         echo 'Supervisor not found';
     }
 } else {
-    echo 'Error: Approver ID not provided';
+    echo 'Error: Supervisor ID not provided';
 }
 ?>
